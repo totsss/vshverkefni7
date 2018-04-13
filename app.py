137c1f987c42a5ca9d56dcc92b19b13447a6852f
totsss <axetra@hotmail.com>
2018-04-13 18:03:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,15 @@ session_opts = {
 app = SessionMiddleware(app(), session_opts)
 
 # Ósköp vejulegt route...
+
 @route('/')
+def index():
+  return """
+            <a href='/innskra'>liður a</a><br>
+            <a href='/versla'>liður b</a><br>
+         """
+
+@route('/versla')
 def index():
   return """
             <a href='/byrja/0'>vara 1</a><br>
@@ -18,6 +26,8 @@ def index():
             <a href='/byrja/2'>vara 3</a><br>
             <a href='/karfa'>karfa</a><br>
             <a href='/eyda'>Eyða Session</a><br>
+            <a href='/'>liðir</a><br>
+
          """
 # Búum til session
 
@@ -27,18 +37,22 @@ def byrja(id):
     s = request.environ.get('beaker.session')
     s[breyta] = "gg"
     s.save()
-    return """vara hefur verið sett i körfu <br><a href='/'>Heim</a>"""
+    return """vara hefur verið sett i körfu <br> <a href='/versla'>Heim</a>"""
 
-# Athugum hvort tiltekið session sé til
+
 @route('/karfa')
 def karfa():
     s = request.environ.get('beaker.session')
-
+    vorulisti = str()
     # Athugum hvort session-ið prufa sé til
-    if s.get('gg'):
-        return s,"vara til til <br><a href='/'>Heim</a>"
-    else:
-        return "ekkrt til í körfu <br><a href='/'>Heim</a>"
+    for i in range(4):
+        if s.get(str(i)):
+            strengur = "vara" + str(i+1)
+            vorulisti = vorulisti + strengur
+    for s in vorulisti:
+        return vorulisti,"<br><a href='/versla'>vorur</a>"
+
+
 
 # Eyðum session
 @route('/eyda')
@@ -47,9 +61,39 @@ def eyda():
     s = request.environ.get('beaker.session',)
     s.delete()
 
-    return "Session (prufa) eytt!<br><a href='/'>Heim</a>"
+    return "Session (prufa) eytt!<br><a href='/versla'>Heim</a>"
     #else:
      #   return "Þessi vara var ekki til áður !"
 
-# Muna eftir app=app í run fallinu
-run(app=app)
+
+
+from beaker.middleware import SessionMiddleware
+
+@route('/innskra')
+def start():
+	return template("innskra.tpl")
+
+@post("/data")
+def data():
+
+	n = request.forms.get("nafn")
+	p = request.forms.get("password")
+	#response.set_cookie("kokunanf", "admin")
+	#response.set_cookie("kokupassword", "admin")
+	print(n,p)
+
+	if n == "admin" and p == "1234":
+		response.set_cookie("admin", "ok")
+		redirect("/leyni")
+	else:
+		return ("Þú hefur ekki aðgang...")
+
+@route("/leyni")
+def leyno():
+	if request.get_cookie("admin","ok"):
+		return "Þú ert komin á leynisíðunna..."
+
+
+
+
+run(app=app,host='0.0.0.0',port=os.environ.get('PORT'))
